7feabc4378de32b72819ac76df8753866a06c29f
daniel <dkinzler@wikimedia.org>
2021-03-09 22:36:31 +0100

    Pass a database to acquireActorId().
    Calling acquireActorId() without passing a database connection is going
    to be deprecated soon.
    
    Bug: T276986
    Needed-By: Ie8aadaffa4a30e63fcad0502d45b8ec364fc79ef
    Change-Id: I774015f81af2c2456dec329f35beb70ff4d71a5b

--- maintenance/migrateOldVoteUserColumnsToActor.php
+++ maintenance/migrateOldVoteUserColumnsToActor.php
@@ -66,7 +66,7 @@ class MigrateOldVoteUserColumnsToActor extends LoggedUpdateMaintenance {
 			if ( $user->getId() != 0 ) {
 				if ( interface_exists( '\MediaWiki\User\ActorNormalization' ) ) {
 					// MW 1.36+
-					$actorId = MediaWikiServices::getInstance()->getActorNormalization()->acquireActorId( $user );
+					$actorId = MediaWikiServices::getInstance()->getActorNormalization()->acquireActorId( $user, $dbw );
 				} else {
 					$actorId = $user->getActorId( $dbw );
 				}
